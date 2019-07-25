31de9e7d8da6d976acfa6de9bb230b09ea023b1f
RAIHAN <darkh6824@gmail.com>
2019-07-25 11:18:01 +0700

    Latihan2

---
+++ app/src/main/java/com/example/mylatihancontrain/latihanConstrainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mylatihancontrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class latihanConstrainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan_constrain);
+    }
+}
